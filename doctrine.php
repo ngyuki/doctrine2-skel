6c1f6d3c4d9f9b6e942037884ea85634cd429978
tsyk goto <ngyuki.ts@gmail.com>
2012-10-29 11:20:06 +0900

    doctrine.php

--- doctrine.php
+++ doctrine.php
@@ -1,25 +1,18 @@
 <?php
-use Doctrine\ORM\Tools\Setup;
-use Doctrine\ORM\EntityManager;
-use Doctrine\ORM\Tools\Console\Helper\EntityManagerHelper;
-use Doctrine\DBAL\Tools\Console\Helper\ConnectionHelper;
-use Symfony\Component\Console\Helper\HelperSet;
-use Symfony\Component\Console\Helper\DialogHelper;
-
 require_once __DIR__ . '/vendor/autoload.php';
 
 $isDevMode = true;
-$config = Setup::createXMLMetadataConfiguration(array(__DIR__ . '/xml'), $isDevMode);
+$config = \Doctrine\ORM\Tools\Setup::createXMLMetadataConfiguration(array(__DIR__ . '/xml'), $isDevMode);
 
 $params = include __DIR__ . '/db-params.php';
 
-$em = EntityManager::create($params, $config);
+$em = \Doctrine\ORM\EntityManager::create($params, $config);
 $db = $em->getConnection();
 
-$helperSet = new HelperSet(array(
-	'em' => new EntityManagerHelper($em),
-	'db' => new ConnectionHelper($db),
-	'dialog' => new DialogHelper(),
+$helperSet = new \Symfony\Component\Console\Helper\HelperSet(array(
+	'em' => new \Doctrine\ORM\Tools\Console\Helper\EntityManagerHelper($em),
+	'db' => new \Doctrine\DBAL\Tools\Console\Helper\ConnectionHelper($db),
+	'dialog' => new \Symfony\Component\Console\Helper\DialogHelper(),
 ));
 
 spl_autoload_register(function($name){
